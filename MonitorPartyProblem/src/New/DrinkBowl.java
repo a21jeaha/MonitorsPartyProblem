5403568312e14650dca3f1ce071435f7a37c87bf
Jean-Paul <a21jeaha@student.his.se>
2022-11-20 16:45:15 +0100

    not working

--- MonitorPartyProblem/src/New/DrinkBowl.java
+++ MonitorPartyProblem/src/New/DrinkBowl.java
@@ -11,22 +11,36 @@ public class DrinkBowl {
     }
 
     // host fills bowl
-    public synchronized int fillBowl() {
-        // fillRequestSent = COME BACK TO THIS
-        return this.drinksInBowl = 3;
+    public synchronized void fillBowl(int drinkFiller, Boolean isHost) {
+        if (drinksInBowl > 0 && isHost == true) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace(); // IN TESTING
+            }
+            this.drinksInBowl = drinkFiller;
+        } else {
+            this.notify();
+        }
     }
 
     // guest takes a drink
-    public synchronized boolean getDrink() {
+    public synchronized int getDrink() {
         // does bowl need to be filled?
-        if (drinksInBowl <= 1) {
-            if (fillRequestSent == false) {
-                fillRequestSent = true;
-                fillBowl();
+        if (drinksInBowl <= 1 && fillRequestSent == false) {
+
+            fillRequestSent = true;
+            fillBowl(0, false);
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        } else {
-            return false;
         }
+        drinksInBowl--;
+        return 1;
     }
 
 }
